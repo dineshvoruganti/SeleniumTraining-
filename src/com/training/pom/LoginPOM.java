9e5ad856201e7d77e59eac991bdb28813be8b67a
dineshvoruganti <dvorugan@in.ibm.com>
2020-01-08 21:57:36 -0500

    My First commit

---
+++ src/com/training/pom/LoginPOM.java
@@ -0,0 +1,44 @@
+package com.training.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPOM {
+	private WebDriver driver; 
+	
+	public LoginPOM(WebDriver driver) {
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//a[contains(text(), ' Log In / Register')]")
+	private WebElement loginPage;		
+	
+	@FindBy(name="log")
+	private WebElement userName; 
+	
+	@FindBy(name="pwd")
+	private WebElement password;
+	
+	@FindBy(name="login")
+	private WebElement loginBtn; 
+	
+	public void loginLink(){
+		this.loginPage.click();
+	}
+	public void sendUserName(String userName) {
+		this.userName.clear();
+		this.userName.sendKeys(userName);
+	}
+	
+	public void sendPassword(String password) {
+		this.password.clear(); 
+		this.password.sendKeys(password); 
+	}
+	
+	public void clickLoginBtn() {
+		this.loginBtn.click(); 
+	}
+}
---
+++ src/com/training/utility/Driver.java
@@ -0,0 +1,16 @@
+package com.training.utility;
+
+public interface Driver {
+	// KEYS 
+	String CHROME ="webdriver.chrome.driver"; 
+	String FIREFOX ="webdriver.firefox.marionette"; 
+	String IE ="webdriver.ie.driver";
+	String PHANTOM="phantomjs.binary.path";
+	
+	// PATH 
+	String CHROME_PATH="C:\\Users\\DineshVoruganti\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe";
+	String FIREFOX_PATH ="C:\\Users\\DineshVoruganti\\Downloads\\Selenium\\geckodriver-v0.26.0-win32\\geckodriver.exe";
+	String IE_PATH ="";
+	String PHANTOM_PATH="";
+	
+}
